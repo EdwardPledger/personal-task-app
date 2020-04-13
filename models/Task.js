58fd876b8f26e11f00832feefd3633d418b51db2
Edward Pledger <edward.l.pledger@gmail.com>
2020-04-13 12:25:42 -0500

    Updated folder structure and included style guide

--- models/Task.js
+++ models/Task.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+
+const [Schema] = mongoose;
 
 const taskSchema = new Schema({
-    actualTime: Number,
-    taskState: Boolean,
-    name: String,
-    description: String,
-    estimatedTime: Number
+  actualTime: Number,
+  taskState: Boolean,
+  name: String,
+  description: String,
+  estimatedTime: Number,
 },
 {
-    collection: 'task'
-})
+  collection: 'task',
+});
 
-module.exports = mongoose.model('Task', taskSchema)
+module.exports = mongoose.model('Task', taskSchema);
--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-const Koa = require('koa')  // TODO: convert to ES6 module?
-const mongoose = require('mongoose')
-const Task = require('./models/Task')
+const Koa = require('koa'); // TODO: convert to ES6 module?
+const mongoose = require('mongoose');
+const Task = require('./models/Task');
 
 /**
  * Server configuration
  */
-const app = new Koa()
+const app = new Koa();
 
-app.use(async context => {
-    context.body = 'Hello, World!'
-})
-app.listen(3000, () => console.log('Koa is listening on port 3000'))
+app.use(async (context) => {
+  // eslint-disable-next-line no-param-reassign
+  context.body = 'Hello, World!';
+});
+app.listen(3000, () => console.log('Koa is listening on port 3000'));
 
 /**
  * Database configuration
  */
-const url = 'mongodb://localhost/TaskApp'
+const url = 'mongodb://localhost/TaskApp';
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(url, { useNewUrlParser: true });
 
-const db = mongoose.connection
-db.once('open', _ => console.log(`Database connected: ${url}`))
-db.on('error', error => console.log(`Connection error: ${error}`))
+const db = mongoose.connection;
+db.once('open', () => console.log(`Database connected: ${url}`));
+db.on('error', (error) => console.log(`Connection error: ${error}`));
 
 /**
  * Test code
  */
 const test = new Task({
-    name: 'NODE TEST',
-    description: 'NODE DESCRIPTION'
-})
+  name: 'NODE TEST',
+  description: 'NODE DESCRIPTION',
+});
 
 test.save((error, document) => {
-    if (error) console.error(error)
-    console.log(document)
-})
+  if (error) console.error(error);
+  console.log(document);
+});
