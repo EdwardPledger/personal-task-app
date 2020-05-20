5c7c153b6733538a0824875f33eb79966360e633
Edward Pledger <edward.l.pledger@gmail.com>
2020-05-20 17:29:47 -0500

    Started front end pick date/time for task

--- task-app/src/app/app.module.ts
+++ task-app/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DailyPlannerComponent } from './daily-planner/daily-planner.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';   
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -18,9 +25,18 @@ import { DailyPlannerComponent } from './daily-planner/daily-planner.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule
   ],
   providers: [
   ],
--- task-app/src/app/tasks/tasks.component.ts
+++ task-app/src/app/tasks/tasks.component.ts
@@ -12,6 +12,7 @@ export class TasksComponent implements OnInit {
   task: Task;
   tasks: Task[];
   selectedTask: Task;
+  times: Number[] = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   constructor(
     private taskService: TaskService,
